ac1f9dd1316fa3f64d0660680e90f4e3534df61a
alextappin <alextappin@gmail.com>
2016-04-16 04:36:34 -0700

    Added balls but text isnt working right now.

--- scripts/gameLoop/playGame.js
+++ scripts/gameLoop/playGame.js
@@ -10,9 +10,19 @@ PlayGame.prototype.constructPlayGame = function(stage) {
     this.ball = new Ball();
     this.ball.position.x = 300;
     this.ball.position.y = 400;
+    this.ball.scale.x = .8;
+    this.ball.scale.y = .8;
+
+    this.ball2 = new Ball();
+    this.ball2.position.x = 200;
+    this.ball2.position.y = 100;
+    this.ball2.scale.x = 1.2;
+    this.ball2.scale.y = 1.2;
     stage.addChild(this.ball);
+    stage.addChild(this.ball2);
 };
 
 PlayGame.prototype.update = function() {
     this.ball.update(this.ball);
+    this.ball2.update(this.ball2);
 };
