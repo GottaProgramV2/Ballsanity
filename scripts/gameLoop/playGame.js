881e69756ed7a3ee7eaf625d650c927b9d557a3b
alextappin <alextappin@gmail.com>
2016-04-16 04:20:04 -0700

    working on the new setup...

---
+++ scripts/gameLoop/playGame.js
@@ -0,0 +1,18 @@
+/**
+ * Created by ajt on 4/16/2016.
+ */
+function PlayGame(stage) {
+    this.constructPlayGame(stage);
+}
+
+PlayGame.prototype.constructPlayGame = function(stage) {
+    //Add to stage the units
+    this.ball = new Ball();
+    this.ball.position.x = 200;
+    this.ball.position.y = 400;
+    stage.addChild(this.ball);
+};
+
+PlayGame.prototype.update = function() {
+    this.ball.update(this.ball);
+};
