2d4e58132e3fc08048c70fde5831c418b0551303
t_tappa <alex.tappin@autodesk.com>
2016-05-04 16:26:30 -0700

    trampoline frame tip

--- scripts/unitSprites/trampolines.js
+++ scripts/unitSprites/trampolines.js
@@ -3,7 +3,7 @@
  */
  
  function Trampoline() {
-     this.texture = PIXI.Texture.fromFrame("black_line.svg");
+     this.texture = PIXI.Texture.fromFrame("black_line.svg");//black_line.svg isnt a frame, its the image name with extension. look in the .json file to see what the frame name is
      this.line = new PIXI.Sprite(this.texture);
  }
  
