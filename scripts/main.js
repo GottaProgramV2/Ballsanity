fd99fdc5d2bddf97ee8ca07c105312cdc149ddf8
nguyensalot <nguyenanthony2014@yahoo.com>
2016-05-09 20:15:29 +0000

    cleaned up existing code

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,32 @@
 /**
  * Created by ajt on 4/16/2016.
- * description: This script will be handling the creation of the canvas for the game and loading of objects. 
  */
+ 
 function Main() {
     var width = 600,
         height = 800;
     this.renderer = PIXI.autoDetectRenderer(width, height, {backgroundColor: 0x1099bb});
     document.body.appendChild(this.renderer.view);
-    this.stage = new PIXI.Container(0x66FF99);
+    this.stage = new PIXI.Container(0x1099bb);
+    
+    this.mousePos = this.renderer.plugins.interaction.mouse.global;
+    
     //load the spriteSheets and stuff...
     this.loadSpriteSheet();
 }
 Main.prototype.loadSpriteSheet = function() {
     //no assets right now...
-    var assetsToLoad = ["../res/ball.png","../res/ball.json"],
+    var assetsToLoad = ["../res/ball.png","../res/ball.json", 
+    "../res/simple_background.png", "../res/simple_background.json", 
+    "../res/black_line.png", "../res/black_line.json"],
         loader = new PIXI.loaders.Loader();
     //this will load the assets to load, then call the spriteSheetLoaded function once finished...
-    loader.add(assetsToLoad).load(this.spriteSheetLoaded.bind(this))
+    loader.add(assetsToLoad).load(this.spriteSheetLoaded.bind(this));
 };
 //make an instance of our PlayGame class
 //request Animation frame will call the update function
 Main.prototype.spriteSheetLoaded = function() {
-    this.playGame = new PlayGame(this.stage);
+    this.playGame = new PlayGame(this.stage, this.mousePos);
     requestAnimationFrame(this.update.bind(this));
 };
 //update will now update the game and render it, then request another update frame.
@@ -31,8 +36,13 @@ Main.prototype.update = function() {
     requestAnimationFrame(this.update.bind(this));
 };
 
-// TODO: separate this into it's own script
 Main.prototype.newGame = function() {
-    for (var i = this.stage.children.length - 1; i >= 0; i--) {	this.stage.removeChild(this.stage.children[i]); };
-    this.playGame = new PlayGame(this.stage);
+    this.stage.destroy();
+    this.stage = new PIXI.Container(0x1099bb);
+    this.playGame = new PlayGame(this.stage, this.mousePos);
+};
+
+Main.prototype.addBall = function() {
+    this.playGame.addBall();
 }
+
